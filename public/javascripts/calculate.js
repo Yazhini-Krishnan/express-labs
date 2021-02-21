6cff130601bb52ef819b32863a5146ce328f74e0
Yazhini-Krishnan <68785790+Yazhini-Krishnan@users.noreply.github.com>
2021-02-21 10:47:43 +0530

    nav_bar

--- public/javascripts/calculate.js
+++ public/javascripts/calculate.js
@@ -1,5 +1,3 @@
-const Finance = require("./finance");
-
 function calculate() {
     var principal = get("principalamor").value;
     var interest = get("interestamor").value;
@@ -8,12 +6,63 @@ function calculate() {
 
     var finance = new Finance();
     var payment = finance.AM(principal, interest, term)
+    finalamount.textContent = payment;
 
 
 }
+
+function calculate1() {
+    var rate = get("fvrate").value;
+    var cashflow = get("fvcashflow").value;
+    var termfv = get("fvterm").value;
+    var finalamount1 = get("finalamount1")
+
+    var finance = new Finance();
+    var payment = finance.FV(rate, cashflow, termfv)
+    finalamount1.textContent = payment;
+
+}
+
+function showcalc() {
+
+    var amorcalc = get("amor");
+    var fvcalc = get("fv");
+    var cagrcalc = get("cagr");
+    hide(amorcalc);
+    hide(fvcalc);
+    hide(cagrcalc);
+    var selectelement = get("calculatorOptions");
+    var selectvalue = selectelement.value;
+    if (selectvalue == 1) {
+        console.log("show amortization");
+        show(amorcalc);
+
+
+
+    } else if (selectvalue == 2) {
+        console.log("show fv");
+        show(fvcalc);
+    } else {
+        console.log("show cagr")
+        show(cagrcalc);
+    }
+
+}
+
+
 
 function get(id) {
     return document.getElementById(id);
 
 
+}
+
+function show(el) {
+    el.style.display = "";
+
+
+}
+
+function hide(el) {
+    el.style.display = "none";
 }
